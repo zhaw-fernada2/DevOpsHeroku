36c53f8fed52b2ba6fe0b029b1a2eaa83181ea6e
zhaw-fernada2 <43756261+zhaw-fernada2@users.noreply.github.com>
2022-05-16 10:06:20 +0200

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello DevOps Course on Tuesday 11th May 2022 :-)');
+  res.send('Hello DevOps Paperlapap');
 });
 
 app.listen(PORT, HOST);
