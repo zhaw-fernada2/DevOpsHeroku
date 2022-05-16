86bfdd357db9af71966a4ce38a8c319511fa9da6
zhaw-fernada2 <43756261+zhaw-fernada2@users.noreply.github.com>
2022-05-16 15:39:47 +0200

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello DevOps Paperlapap');
+  res.send('Hello DevOps-Welt, Heroku deployment hat funktioniert');
 });
 
 app.listen(PORT, HOST);
